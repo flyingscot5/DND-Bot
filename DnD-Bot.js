0cbfbba0b49687a97cf42923bab433be09e8aa96
BuildTools <unconfigured@null.spigotmc.org>
2018-01-11 19:51:14 -0330

    fixed bot

--- DnD-Bot.js
+++ DnD-Bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const fs = require('fs');
+
 const config = require("./Config.json");
 const token = require("./Tokens.json");
 
