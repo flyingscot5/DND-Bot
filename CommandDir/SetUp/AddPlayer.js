85996905c52154065d577a0df444238fc3769cb4
flyingscot5 <callumjpb@yahoo.co.uk>
2018-01-11 21:47:42 +0000

    simple db stuff

--- CommandDir/SetUp/AddPlayer.js
+++ CommandDir/SetUp/AddPlayer.js
@@ -3,21 +3,24 @@ const db = new sqlite3.Database('DnD-Bot.db');
 
 exports.run = function(bot, message, params) {
 
-  db.get(`SELECT * FROM Accounts WHERE UserID=${message.author.id}`, function (err, row) {
-    console.log(row);
-  })
+  var Create = db.prepare("INSERT INTO Accounts VALUES (?)");
+  Create.run(0),
+    function(err, row) {
+      if (err)
+        console.log(err)
+    }
 
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: true,
-  aliases: ['inv', 'backpack'],
+  aliases: [],
   permLevel: 5
 };
 
 exports.help = {
-  name: 'inventory',
+  name: 'exmaple',
   description: 'Example',
   usage: 'Example'
 };
