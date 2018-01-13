883c760f686c6e77493d6fdff75f7d1446b73aa6
flyingscot5 <callumjpb@yahoo.co.uk>
2018-01-13 23:16:36 +0000

    Merge branch 'master' of https://github.com/flyingscot5/DND-Bot

---
+++ CommandDir/Player/BackPack.js
@@ -0,0 +1,17 @@
+exports.run = function(bot, message, params) {
+
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['bp'],
+  permLevel: 5
+};
+
+exports.help = {
+  name: 'backpack',
+  description: 'Example',
+  usage: 'Example'
+};
