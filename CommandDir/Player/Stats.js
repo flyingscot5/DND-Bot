b24dac4f8c47193d536e7d917b12d4ad8ba880f2
flyingscot5 <callumjpb@yahoo.co.uk>
2018-01-13 23:06:17 +0000

    little change

--- CommandDir/Player/Stats.js
+++ CommandDir/Player/Stats.js
@@ -12,12 +12,12 @@ exports.run = function(bot, message, params) {
     var Embed = new Discord.RichEmbed()
       .setAuthor('Your Stats: ')
       .setColor('#ffffff')
-      .addField(`Strength Total: ${Math.round(result.StrTotal/2)}`,`(PlayerStr: ${result.StrBonus} + RaceStr: ${result.raceStrBonus} + ClassStr: ${result.classStrBonus} / 2)`)
-      .addField(`Dexerity Total: ${Math.round(result.DexTotal/2)}`,`(PlayerDex: ${result.DexBonus} + RaceDex: ${result.raceDexBonus} + ClassDex: ${result.classDexBonus} / 2)`)
-      .addField(`Constitution Total: ${Math.round(result.ConTotal/2)}`,`(PlayerCon: ${result.ConBonus} + RaceCon: ${result.raceConBonus} + ClassCon: ${result.classConBonus} / 2)`)
-      .addField(`Intelligence Total: ${Math.round(result.IntTotal/2)}`,`(PlayerInt: ${result.IntBonus} + RaceInt: ${result.raceIntBonus} + ClassInt: ${result.classIntBonus} / 2)`)
-      .addField(`Wisdom Total: ${Math.round(result.WisTotal/2)}`,`(PlayerWis: ${result.WisBonus} + RaceWis: ${result.raceWisBonus} + ClassWis: ${result.classWisBonus} / 2)`)
-      .addField(`Charisma Total: ${Math.round(result.ChaTotal/2)}`,`(PlayerCha: ${result.ChaBonus} + RaceCha: ${result.raceChaBonus} + ClassCha: ${result.classChaBonus} / 2)`)
+      .addField(`Strength Total: ${Math.round(result.StrTotal/2)}`,`(PlayerStr: ${result.StrBonus} + RaceStr: ${result.raceStrBonus} + ClassStr: ${result.classStrBonus}) 2`)
+      .addField(`Dexerity Total: ${Math.round(result.DexTotal/2)}`,`(PlayerDex: ${result.DexBonus} + RaceDex: ${result.raceDexBonus} + ClassDex: ${result.classDexBonus}) 2`)
+      .addField(`Constitution Total: ${Math.round(result.ConTotal/2)}`,`(PlayerCon: ${result.ConBonus} + RaceCon: ${result.raceConBonus} + ClassCon: ${result.classConBonus} ) 2`)
+      .addField(`Intelligence Total: ${Math.round(result.IntTotal/2)}`,`(PlayerInt: ${result.IntBonus} + RaceInt: ${result.raceIntBonus} + ClassInt: ${result.classIntBonus}) / 2`)
+      .addField(`Wisdom Total: ${Math.round(result.WisTotal/2)}`,`(PlayerWis: ${result.WisBonus} + RaceWis: ${result.raceWisBonus} + ClassWis: ${result.classWisBonus}) / 2`)
+      .addField(`Charisma Total: ${Math.round(result.ChaTotal/2)}`,`(PlayerCha: ${result.ChaBonus} + RaceCha: ${result.raceChaBonus} + ClassCha: ${result.classChaBonus}) / 2`)
     message.channel.send(Embed);
   });
 };
